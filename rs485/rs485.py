3dcbfb043070d98cdfe9474cb5f26739fbaa0a55
justleen <leen.smit@gmail.com>
2017-08-30 15:34:05 +0200

    fixing eqid

--- rs485/rs485.py
+++ rs485/rs485.py
@@ -8,7 +8,6 @@ log = logging.getLogger(__name__)
 
 class rsReader(object):
 	defaults = {
-	'eqid' : None,
 	'device': '/dev/ttyUSB0',
 	'devicenumber': 1,
 	'baudrate': 9600,
@@ -31,10 +30,9 @@ class rsReader(object):
 		self.rs485.serial.bytesize = int(config['bytesize'])
 		self.rs485.serial.stopbits = int(config['stopbits'])
 		self.rs485.serial.timeout = int(config['timeout'])
-		self.eqid=config['eqid']
 
 
-	def readRS485(self ):
+	def readRS485(self, eqid ):
 		bodyTemplate_solar = 'emeter_solar,eqid={eqid},type={type} value={value}\n'
 		''' read DSM120 powermeter over rs485 '''
 		ret = {}
@@ -49,8 +47,7 @@ class rsReader(object):
 			ret['sol_nrg'] = abs(float(TotalPower))
 
 			log.debug('Values read: %s and %s',  ret['sol_pow'] , ret['sol_nrg'] ) 
-			self.body  = bodyTemplate_solar.format(eqid=self.eqid,type='cumulative', value=ret['sol_nrg'])
-			self.body += bodyTemplate_solar.format(eqid=self.eqid,type='instant', value=ret['sol_pow'])
+			self.body  = bodyTemplate_solar.format(eqid=eqid,type='cumulative', value=ret['sol_nrg'])
+			self.body += bodyTemplate_solar.format(eqid=eqid,type='instant', value=ret['sol_pow'])
 
 		return self.body
-
